a1ada1c73d3a96ac758aa268135cd67819b6efab
Sean LaPlante <laplante.sean@gmail.com>
2024-11-05 21:10:29 -0500

    Movement tweaks

--- game/levels/level.gd
+++ game/levels/level.gd
@@ -1,2 +1,21 @@
 extends Node2D
 class_name Level
+
+var initialized: bool = false : set = set_initialized
+
+@onready var start_position: Marker2D = $StartPosition
+
+func _physics_process(delta: float) -> void:
+    if initialized:
+        return
+
+    var player: = MainInstances.player as Player
+    if not player or not is_instance_valid(player):
+        return
+
+    initialized = true
+    player.global_position = start_position.global_position
+
+func set_initialized(value: bool) -> void:
+    initialized = value
+    set_physics_process(not value)
--- game/player/states/player_move_state.gd
+++ game/player/states/player_move_state.gd
@@ -1,12 +1,14 @@
 extends State
 class_name PlayerMoveState
 
+const COYOTE_JUMP_REACTION_TIME: = 0.2
+
 signal slide()
 signal crouch()
 
 var just_jumped: bool = false
 var double_jump: bool = true
-var coyote_jump_timer: Timer
+var coyote_jump_timer: SceneTreeTimer
 
 func get_input_vector(player: Player) -> Vector2:
     var input_vector: = Vector2.ZERO
@@ -28,6 +30,9 @@ func jump_check(player: Player) -> void:
         just_jumped = true
     elif (player.is_on_floor() or (coyote_jump_timer and coyote_jump_timer.time_left > 0)) and jump_just_pressed:
         # Regular jump
+        print("regular")
+        if coyote_jump_timer and coyote_jump_timer.time_left > 0:
+            print("coyote saved you")
         jump(player, player.movement_stats.ground_jump_force)
         just_jumped = true
     else:
@@ -36,43 +41,59 @@ func jump_check(player: Player) -> void:
 
         if jump_just_pressed and double_jump == true:
             # Handle double jump
+            print("double")
             jump(player, player.movement_stats.air_jump_force)
             double_jump = false
 
-func apply_horizontal_force(player: Player, delta: float) -> void:
-    if player.input_vector != Vector2.ZERO:
-        var new_velocity = player.direction * player.movement_stats.ground_max_speed
-        player.velocity = player.velocity.move_toward(new_velocity, player.movement_stats.ground_acceleration * delta)
+func apply_horizontal_force(player: Player, input_vector: Vector2, delta: float) -> void:
+    var accel: = player.movement_stats.ground_acceleration
+    var friction: = player.movement_stats.ground_friction
+    var speed: = player.movement_stats.ground_max_speed
+    if not player.is_on_floor() and not player.is_on_wall():  # Air
+        accel = player.movement_stats.air_acceleration
+        friction = player.movement_stats.air_friction
+        speed = player.movement_stats.air_max_speed
+
+    if input_vector != Vector2.ZERO:
+        var new_velocity: = player.direction * speed
+        player.velocity = player.velocity.move_toward(new_velocity, accel * delta)
     else:
-        player.velocity = player.velocity.move_toward(Vector2.ZERO, player.movement_stats.ground_friction * delta)
+        player.velocity = player.velocity.move_toward(Vector2.ZERO, friction * delta)
 
-func process_state(delta: float) -> void:
-    var player: = actor as Player
-    var input_vector: = get_input_vector(player)
-
-    apply_horizontal_force(player, delta)
-    jump_check(player)
-    apply_gravity(delta)
-    update_animations(input_vector)
-    move()
-    wall_slide_check()
-    double_press_check()
-    dig_check()
-
-    # Handle horizontal velocity
-    
-
-    # Gravity
+func apply_gravity(player: Player, delta: float) -> void:
     player.velocity.y += player.movement_stats.gravity * delta
     player.velocity.y = min(player.velocity.y, player.movement_stats.terminal_velocity)
 
-    if player.is_on_wall_only():
-        wall_slide.emit()
-    if not player.is_on_floor() and not player.is_on_wall():
-        print("AIR")
+func move(player: Player):
+    var was_in_air: = not player.is_on_floor()
+    var was_on_floor: = player.is_on_floor()
+    var last_velocity: = Vector2(player.velocity)
+    var last_position: = player.position
 
-    if player.facing_direction.x != 0:
-        player.flip_anchor.scale.x = sign(facing_direction.x)
-
-    # Move
     player.move_and_slide()
+
+    # Happens on landing
+    if was_in_air and player.is_on_floor():
+        print("land")
+        # On landing we get double jump back
+        double_jump = true
+
+    # Just left the ground
+    if was_on_floor and not player.is_on_floor() and not just_jumped:
+        coyote_jump_timer = player.get_tree().create_timer(COYOTE_JUMP_REACTION_TIME)
+
+func update_animations(player: Player) -> void:
+    if player.facing_direction.x != 0:
+        player.flip_anchor.scale.x = sign(player.facing_direction.x)
+
+func process_state(delta: float) -> void:
+    just_jumped = false
+    var player: = actor as Player
+    var input_vector: = get_input_vector(player)
+
+    apply_horizontal_force(player, input_vector, delta)
+    jump_check(player)
+    apply_gravity(player, delta)
+    update_animations(player)
+    move(player)
+    #wall_slide_check()

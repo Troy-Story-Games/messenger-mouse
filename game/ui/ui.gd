a1991b6cbd33a9514f37a20a60622753cc2d59d8
Sean LaPlante <laplante.sean@gmail.com>
2024-11-09 20:36:58 -0500

    Fonts and timer polish

--- game/ui/ui.gd
+++ game/ui/ui.gd
@@ -2,26 +2,30 @@ extends CanvasLayer
 class_name UI
 
 var running: bool = false
-var time: = 0.0 : set = set_time
+var time_ms: = 0.0 : set = set_time_ms
 
 @onready var clock: Label = $Control/Clock
 
 func start_timer() -> void:
-    time = 0.0
+    time_ms = 0.0
     running = true
 
 func stop_timer() -> float:
-    var ret: = time
-    time = 0.0
+    var ret: = time_ms
+    time_ms = 0.0
     running = false
     return ret
 
 func _process(delta: float) -> void:
     if not running:
         return
-    time += delta
+    time_ms += delta * 1000
 
-func set_time(value: float) -> void:
-    time = value
-    # TODO: minute:second
-    clock.text = str(int(value)) + "s"
+func set_time_ms(value: float) -> void:
+    time_ms = value
+    var rem_ms: = time_ms
+    var rem_m: = int(floor((rem_ms / 1000.0) / 60.0))
+    rem_ms -= rem_m * 60 * 1000
+    var rem_s: = int(floor(rem_ms / 1000.0))
+    rem_ms -= rem_s * 1000
+    clock.text = "%02d:%02d:%02d" % [rem_m, rem_s, int(floor(rem_ms / 10.0))]

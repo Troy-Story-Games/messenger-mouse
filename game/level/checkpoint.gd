08f6755d4ffc370d5af1e948ae2836367fd23b20
Sean LaPlante <laplante.sean@gmail.com>
2024-11-20 22:57:23 -0500

    Checkpoint polish

--- game/level/checkpoint.gd
+++ game/level/checkpoint.gd
@@ -1,9 +1,13 @@
 extends Area2D
 class_name Checkpoint
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 func _ready() -> void:
     area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area: Area2D) -> void:
+    animation_player.play("deploy")
+    SoundFx.play("checkpoint", 1, -10, 0)
     Events.player_checkpoint.emit(global_position)
     set_deferred("monitoring", false)

fa04fd746dd8d765cf20fe40a16fb513f7c99b7a
BingBongBang <travis.constantino@gmail.com>
2024-11-23 11:27:25 -0800

    in progress level stuff

--- game/world/world.gd
+++ game/world/world.gd
@@ -4,7 +4,7 @@ class_name World
 const PlayerScene = preload("res://game/player/player.tscn")
 
 var current_level: Level
-var current_level_idx: int = 2
+var current_level_idx: int = 3
 var zoom_out_enabled: bool = false
 var last_checkpoint: Vector2
 

7f6efd784a8850599abdbd5af0ee3df6c6884b67
Sean LaPlante <laplante.sean@gmail.com>
2024-11-26 16:14:27 -0500

    UI improvements

--- game/fx/player_hurt_flash.gd
+++
@@ -1,7 +0,0 @@
-extends CanvasLayer
-class_name PlayerHurtFlash
-
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-func _ready() -> void:
-	Events.player_hurt.connect(animation_player.play.bind("flash"))

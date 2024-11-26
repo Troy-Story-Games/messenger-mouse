a3b80946777d91481a3fea7510f13d76fa05e269
Sean LaPlante <laplante.sean@gmail.com>
2024-11-26 18:34:26 -0500

    flame timer

--- game/ui/ui.gd
+++ game/ui/ui.gd
@@ -5,12 +5,17 @@ signal ui_tutorial_complete()
 
 var running: bool = false
 var time_ms: = 0.0 : set = set_time_ms
+var total_cheats: int = 0 : set = set_total_cheats
+var total_secrets: int = 0 : set = set_total_secrets
+var cheats_found: int = 0 : set = set_cheats_found
+var secrets_found: int = 0 : set = set_secrets_found
 
 @onready var secrets_found_label: Label = $Control/Secrets/HBoxContainer/SecretsFoundLabel
 @onready var cheats_found_label: Label = $Control/Cheats/HBoxContainer/CheatsFoundLabel
 @onready var clock: Label = $Control/Timer/Clock
 @onready var tutorial: MarginContainer = $Control/Tutorial
 @onready var tutorial_animation_player: AnimationPlayer = $TutorialAnimationPlayer
+@onready var flame_progress_bar: ProgressBar = $Control/Flame/HBoxContainer/MarginContainer/FlameProgressBar
 
 func start_timer() -> void:
     time_ms = 0.0
@@ -22,6 +27,14 @@ func stop_timer() -> float:
     running = false
     return ret
 
+func set_flame_progress_max(value: float) -> void:
+    flame_progress_bar.max_value = value
+
+func set_flame_progress(value: float) -> void:
+    flame_progress_bar.value = value
+    # TODO: Tween progress bar y-scale
+    # TODO: Shake progress bar when close to 0
+
 func show_tutorial() -> void:
     tutorial_animation_player.play("tutorial")
 
@@ -31,11 +44,27 @@ func tutorial_finished() -> void:
 func tutorial_step() -> void:
     SoundFx.play("ui_tutorial_step")
 
-func set_total_cheats(count: int) -> void:
-    pass
+func update_cheats_label() -> void:
+    cheats_found_label.text = "%02d/%02d" % [cheats_found, total_cheats]
 
-func set_total_secrets(count: int) -> void:
-    pass
+func update_secrets_label() -> void:
+    secrets_found_label.text = "%02d/%02d" % [secrets_found, total_secrets]
+
+func set_cheats_found(value: int) -> void:
+    cheats_found = value
+    update_cheats_label()
+
+func set_secrets_found(value: int) -> void:
+    secrets_found = value
+    update_secrets_label()
+
+func set_total_cheats(value: int) -> void:
+    total_cheats = value
+    update_cheats_label()
+
+func set_total_secrets(value: int) -> void:
+    total_secrets = value
+    update_secrets_label()
 
 func _process(delta: float) -> void:
     if not running:

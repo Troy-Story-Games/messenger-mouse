38c16567f52f38620581fd4710670ba535154c79
Sean LaPlante <laplante.sean@gmail.com>
2024-11-30 14:34:04 -0500

    Fixes pause menu with escape. Fixes transparency during pause and level end. Adds new campfire. Adds buttons

--- game/player/player.gd
+++ game/player/player.gd
@@ -41,108 +41,108 @@ var dead: bool = false
 @onready var fsm: FSM = FSM.new().set_state(move_state)
 
 func _enter_tree() -> void:
-    MainInstances.player = self
+	MainInstances.player = self
 
 func _ready() -> void:
-    assert(movement_stats, "movement_stats must be set")
-    stats.health = stats.max_health
-    Events.request_camera_target.emit.call_deferred(remote_transform_2d)
-    hurtbox.hurt.connect(_on_hurtbox_hurt)
-    hurtbox.environment_damage.connect(take_environment_hit)
-    sprite_shader_material = sprite_2d.material as ShaderMaterial
-    sprite_shader_material.set_shader_parameter("nb_frames", Vector2(sprite_2d.hframes, sprite_2d.vframes))
-    collection_area_2d.area_entered.connect(collect_item)
-    Events.toggle_cheat.connect(_on_toggle_cheat)
-    move_state.finished.connect(_on_move_state_finished)
-    die_state.finished.connect(_on_die_state_finished)
-    light_flame_state.finished.connect(fsm.change_state.bind(move_state))
-    hitbox.hit_hurtbox.connect(_on_hit_enemy)
-    light_flame_background_mask.hide()
+	assert(movement_stats, "movement_stats must be set")
+	stats.health = stats.max_health
+	Events.request_camera_target.emit.call_deferred(remote_transform_2d)
+	hurtbox.hurt.connect(_on_hurtbox_hurt)
+	hurtbox.environment_damage.connect(take_environment_hit)
+	sprite_shader_material = sprite_2d.material as ShaderMaterial
+	sprite_shader_material.set_shader_parameter("nb_frames", Vector2(sprite_2d.hframes, sprite_2d.vframes))
+	collection_area_2d.area_entered.connect(collect_item)
+	Events.toggle_cheat.connect(_on_toggle_cheat)
+	move_state.finished.connect(_on_move_state_finished)
+	die_state.finished.connect(_on_die_state_finished)
+	light_flame_state.finished.connect(fsm.change_state.bind(move_state))
+	hitbox.hit_hurtbox.connect(_on_hit_enemy)
+	light_flame_background_mask.hide()
 
 func _on_die_state_finished() -> void:
-    Events.player_died.emit()
-    queue_free()
+	Events.player_died.emit()
+	queue_free()
 
 func _on_move_state_finished() -> void:
-    if stats.health <= 0:
-        die()
-    else:
-        fsm.change_state(light_flame_state)
+	if stats.health <= 0:
+		die()
+	else:
+		fsm.change_state(light_flame_state)
 
 func _on_hit_enemy(_enemy_hurtbox: Hurtbox) -> void:
-    SoundFx.play("slice_impact")
+	SoundFx.play("slice_impact")
 
 func _on_toggle_cheat(cheat_name: String) -> void:
-    if cheat_name == "long_trail":
-        if not long_trail:
-            long_trail = true
-            sprite_shader_material.set_shader_parameter("trail_size", 50)
-        else:
-            long_trail = false
-            sprite_shader_material.set_shader_parameter("trail_size", 12)
-    if cheat_name == "sparkle_trail":
-        if not sparkle_trail_enabled:
-            print("SPARKLE TRAIL!")
-            sparkle_trail_enabled = true
-            sparkle_trail.emitting = true
-        else:
-            sparkle_trail_enabled = false
-            sparkle_trail.emitting = false
+	if cheat_name == "long_trail":
+		if not long_trail:
+			long_trail = true
+			sprite_shader_material.set_shader_parameter("trail_size", 50)
+		else:
+			long_trail = false
+			sprite_shader_material.set_shader_parameter("trail_size", 12)
+	if cheat_name == "sparkle_trail":
+		if not sparkle_trail_enabled:
+			print("SPARKLE TRAIL!")
+			sparkle_trail_enabled = true
+			sparkle_trail.emitting = true
+		else:
+			sparkle_trail_enabled = false
+			sparkle_trail.emitting = false
 
 func _process(_delta: float) -> void:
-    sprite_shader_material.set_shader_parameter("frame_coords", sprite_2d.frame_coords)
-    sprite_shader_material.set_shader_parameter("velocity", Vector2(abs(velocity.x), velocity.y))
+	sprite_shader_material.set_shader_parameter("frame_coords", sprite_2d.frame_coords)
+	sprite_shader_material.set_shader_parameter("velocity", Vector2(abs(velocity.x), velocity.y))
 
 func _physics_process(delta: float) -> void:
-    fsm.state.process_state(delta)
+	fsm.state.process_state(delta)
 
 func set_direction(value: Vector2) -> void:
-    if value == Vector2.ZERO:
-        return
-    value = value.normalized()
-    direction = value
+	if value == Vector2.ZERO:
+		return
+	value = value.normalized()
+	direction = value
 
 func set_facing_direction(value: Vector2) -> void:
-    if value == Vector2.ZERO:
-        return
-    value = value.normalized()
-    value = Vector2(sign(value.x), 0)
-    facing_direction = value
+	if value == Vector2.ZERO:
+		return
+	value = value.normalized()
+	value = Vector2(sign(value.x), 0)
+	facing_direction = value
 
 func take_environment_hit() -> void:
-    take_hit(1)
+	take_hit(1)
 
 func _on_hurtbox_hurt(other_hitbox: Hitbox) -> void:
-    take_hit(other_hitbox.damage)
+	take_hit(other_hitbox.damage)
 
 func take_hit(damage: float) -> void:
-    SoundFx.play("player_hurt")
-    hurtbox.is_invincible = true
-    Events.player_hurt.emit()
-    Events.request_camera_screenshake.emit(4, 0.3)
-    stats.health -= damage
-    await get_tree().create_timer(0.5).timeout
-    hurtbox.is_invincible = false
+	SoundFx.play("player_hurt")
+	hurtbox.is_invincible = true
+	Events.player_hurt.emit()
+	Events.request_camera_screenshake.emit(4, 0.3)
+	stats.health -= damage
+	await get_tree().create_timer(0.5).timeout
+	hurtbox.is_invincible = false
 
 func die() -> void:
-    dead = true
-    fsm.change_state(die_state)
+	dead = true
+	fsm.change_state(die_state)
 
 func collect_item(item: Collectible) -> void:
-    item.collect()
+	item.collect()
 
 func is_ceiling_raycast_colliding() -> bool:
-    return ceiling_check_ray_cast_2d.is_colliding()
+	return ceiling_check_ray_cast_2d.is_colliding()
 
 func is_floor_raycast_colliding() -> bool:
-    return floor_check_ray_cast_2d.is_colliding()
+	return floor_check_ray_cast_2d.is_colliding()
 
 func _on_head_jump_area_2d_body_entered(body: Node2D) -> void:
-    var enemy: Enemy = body as Enemy
-    if enemy.head_kill_enemy:
-        # If they're not bouncy, give a little hop
-        if not enemy.bouncy_enemy:
-            velocity.y = 0
-            velocity.y -= movement_stats.enemy_kill_air_boost
-        move_state.double_jump = true
-        enemy.squish()
+	var enemy: Enemy = body as Enemy
+	if enemy.head_kill_enemy:
+		# If they're not bouncy, give a little hop
+		if not enemy.bouncy_enemy:
+			velocity.y = 0
+			velocity.y -= movement_stats.enemy_kill_air_boost
+		move_state.double_jump = true
+		enemy.squish()

a0d4f4804f0a6d1dfd18f8e5d276c61fa8cef40e
Sean LaPlante <laplante.sean@gmail.com>
2024-11-30 12:04:49 -0500

    Beta release

--- game/level/secret.gd
+++ game/level/secret.gd
@@ -13,56 +13,56 @@ var revealed: bool = false : set = set_revealed
 @onready var reveal_area_2d: Area2D = $RevealArea2D
 
 func _ready() -> void:
-	assert(secret_type, "Secret type not set!!")
-	assert(secret_name, "Secret name not set!!")
-	mask_tiles.modulate = Color.WHITE
-	match secret_type:
-		"SecretArea":
-			assert(not secret_data, "Secret data should not be set for secret areas")
-			if secret_name in SaveAndLoad.save_data.secrets:
-				print("Secret ", secret_name, " already found")
-				revealed = true
-				found = true
-		"CheatCode":
-			assert(secret_data, "Cheat codes need secret_data populated with the cheat code resource")
-			if secret_name in SaveAndLoad.save_data.cheats:
-				print("Cheat ", secret_name, " already found")
-				revealed = true
-				found = false  # So it can be re-found and popup the cheat code
+    assert(secret_type, "Secret type not set!!")
+    assert(secret_name, "Secret name not set!!")
+    mask_tiles.modulate = Color.WHITE
+    match secret_type:
+        "SecretArea":
+            assert(not secret_data, "Secret data should not be set for secret areas")
+            if secret_name in SaveAndLoad.save_data.secrets:
+                print("Secret ", secret_name, " already found")
+                revealed = true
+                found = true
+        "CheatCode":
+            assert(secret_data, "Cheat codes need secret_data populated with the cheat code resource")
+            if secret_name in SaveAndLoad.save_data.cheats:
+                print("Cheat ", secret_name, " already found")
+                revealed = true
+                found = false  # So it can be re-found and popup the cheat code
 
-	area_entered.connect(_on_area_entered)
-	reveal_area_2d.area_entered.connect(_on_reveal_area_2d_area_entered)
+    area_entered.connect(_on_area_entered)
+    reveal_area_2d.area_entered.connect(_on_reveal_area_2d_area_entered)
 
 func set_found(value: bool) -> void:
-	found = value
+    found = value
 
 func set_revealed(value: bool) -> void:
-	revealed = value
-	if revealed:
-		mask_tiles.hide()
-	else:
-		mask_tiles.show()
+    revealed = value
+    if revealed:
+        mask_tiles.hide()
+    else:
+        mask_tiles.show()
 
 func found_secret() -> void:
-	match secret_type:
-		"SecretArea":
-			Events.secret_found.emit(self)
-		"CheatCode":
-			SoundFx.play("cheat_code_pickup", 1, -15, 0)
-			Events.cheat_found.emit(self)
+    match secret_type:
+        "SecretArea":
+            Events.secret_found.emit(self)
+        "CheatCode":
+            SoundFx.play("cheat_code_pickup", 1, -15, 0)
+            Events.cheat_found.emit(self)
 
 func _on_area_entered(_area: Area2D) -> void:
-	print("Found by player: ", secret_name)
-	if not found:
-		if not revealed:
-			print("Revealed too!")
-			revealed = true
-			SoundFx.play("secret_discovered_sound", 1, -15, 0)
-		found = true
-		found_secret()
+    print("Found by player: ", secret_name)
+    if not found:
+        if not revealed:
+            print("Revealed too!")
+            revealed = true
+            SoundFx.play("secret_discovered_sound", 1, -15, 0)
+        found = true
+        found_secret()
 
 func _on_reveal_area_2d_area_entered(_area: Area2D) -> void:
-	print("Reveal the secret area")
-	if not revealed:
-		SoundFx.play("secret_discovered_sound", 1, -15, 0)
-		revealed = true
+    print("Reveal the secret area")
+    if not revealed:
+        SoundFx.play("secret_discovered_sound", 1, -15, 0)
+        revealed = true

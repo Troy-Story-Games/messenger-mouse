8939451f41e3352201d576e25ba7bb72f9de99eb
Sean LaPlante <laplante.sean@gmail.com>
2024-12-01 09:36:32 -0500

    Whatever

--- game/damage_boxes/hurtbox.gd
+++ game/damage_boxes/hurtbox.gd
@@ -6,12 +6,12 @@ signal hurt(hitbox: Hitbox)
 signal environment_damage()
 
 var is_invincible := false :
-	set(value):
-		is_invincible = value
-		set_deferred("monitoring", not is_invincible)
+    set(value):
+        is_invincible = value
+        set_deferred("monitoring", not is_invincible)
 
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
+    body_entered.connect(_on_body_entered)
 
 func _on_body_entered(_body: Node2D) -> void:
-	environment_damage.emit()
+    environment_damage.emit()
--- game/ui/level_complete.gd
+++ game/ui/level_complete.gd
@@ -12,48 +12,48 @@ signal next_level()
 @onready var level_name: Label = $CenterContainer/VBoxContainer/LevelName
 
 func _ready() -> void:
-	hide()
-	continue_button.disabled = true
-	Events.next_level.connect(_on_next_level)
+    hide()
+    continue_button.disabled = true
+    Events.next_level.connect(_on_next_level)
 
 func _on_next_level(stats: Dictionary):
-	show()
+    show()
 
-	var current_level: Level = MainInstances.current_level
-	if current_level and is_instance_valid(current_level) and current_level.level_name:
-		level_name.text = current_level.level_name
+    var current_level: Level = MainInstances.current_level
+    if current_level and is_instance_valid(current_level) and current_level.level_name:
+        level_name.text = current_level.level_name
 
-	# Show stats
-	best_time.text = "Best Time: " + Utils.ms_to_string(stats.best_time)
-	current_time.text = "Time: " + Utils.ms_to_string(stats.last_time)
-	secrets.text = "Secrets: %02d/%02d" % [stats.secrets_found, stats.num_secrets]
-	cheats.text = "Cheats: %02d/%02d" % [stats.cheats_found, stats.num_cheats]
+    # Show stats
+    best_time.text = "Best Time: " + Utils.ms_to_string(stats.best_time)
+    current_time.text = "Time: " + Utils.ms_to_string(stats.last_time)
+    secrets.text = "Secrets: %02d/%02d" % [stats.secrets_found, stats.num_secrets]
+    cheats.text = "Cheats: %02d/%02d" % [stats.cheats_found, stats.num_cheats]
 
-	continue_button.disabled = false
-	continue_button.grab_focus()
-	animation_player.play("show")
-	get_tree().set_deferred("paused", true)
+    continue_button.disabled = false
+    continue_button.grab_focus()
+    animation_player.play("show")
+    get_tree().set_deferred("paused", true)
 
 func hide_menu() -> void:
-	Events.request_script_pause.emit(false)
-	hide()
-	animation_player.play(&"RESET")
-	continue_button.disabled = true
+    Events.request_script_pause.emit(false)
+    hide()
+    animation_player.play(&"RESET")
+    continue_button.disabled = true
 
 func _on_continue_button_pressed() -> void:
-	hide_menu()
-	next_level.emit()
+    hide_menu()
+    next_level.emit()
 
 func _on_save_and_quit_button_pressed() -> void:
-	SaveAndLoad.save_game()
-	get_tree().quit(0)
+    SaveAndLoad.save_game()
+    get_tree().quit(0)
 
 func _on_stay_here_button_pressed() -> void:
-	Events.level_stay_here.emit()
-	hide_menu()
+    Events.level_stay_here.emit()
+    hide_menu()
 
 func _on_main_menu_button_pressed() -> void:
-	SaveAndLoad.save_data.next_level_index = SaveAndLoad.save_data.next_level_index + 1
-	SaveAndLoad.save_game()
-	Events.request_script_pause.emit(false)
-	get_tree().change_scene_to_file("res://game/ui/main_menu.tscn")
+    SaveAndLoad.save_data.next_level_index = SaveAndLoad.save_data.next_level_index + 1
+    SaveAndLoad.save_game()
+    Events.request_script_pause.emit(false)
+    get_tree().change_scene_to_file("res://game/ui/main_menu.tscn")

6882feb1c9c3babb2acb29ff36affb9158d50c93
Sean LaPlante <laplante.sean@gmail.com>
2024-11-30 19:03:09 -0500

    Cheat code improvements

--- game/ui/cheat_code_ui.gd
+++ game/ui/cheat_code_ui.gd
@@ -1,7 +1,11 @@
 extends Control
 class_name CheatCodeUI
 
+signal cheat_code_ui_closed()
+
 var viewed_long_enough: bool = false
+
+@export var handle_pause: bool = true
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var cheat_title_label: Label = $CenterContainer/HBoxContainer/CheatTitleLabel
@@ -10,39 +14,50 @@ var viewed_long_enough: bool = false
 @onready var timer: Timer = $Timer
 
 func _ready() -> void:
-	viewed_long_enough = false
-	hide()
+    viewed_long_enough = false
+    hide()
+
+func display_cheat_code_from_dict(cheat_code: Dictionary) -> void:
+    show()
+    viewed_long_enough = false
+    animation_player.play("show")
+    if handle_pause:
+        Events.request_script_pause.emit(true)
+    cheat_title_label.text = cheat_code.name
+    cheat_code_label.text = Utils.generate_cheat_string(cheat_code.button_combo)
 
 func display_cheat_code(cheat_code: CheatCode) -> void:
-	show()
-	animation_player.play("show")
-	Events.request_script_pause.emit(true)
-	cheat_title_label.text = cheat_code.cheat_name
-	cheat_code_label.text = Utils.generate_cheat_string(cheat_code.button_combo)
+    var dict = {
+        "button_combo": cheat_code.button_combo,
+        "name": cheat_code.cheat_name
+    }
+    display_cheat_code_from_dict(dict)
 
 func _process(_delta: float) -> void:
-	if viewed_long_enough:
-		if Input.get_connected_joypads():
-			viewed_long_enough_label.text = "A to continue"
-		else:
-			viewed_long_enough_label.text = "SPACE to continue"
-		return
-	var show_time = 4
-	if not timer.is_stopped():
-		show_time = timer.time_left
-	viewed_long_enough_label.text = "Look! %02d" % show_time
+    if viewed_long_enough:
+        if Input.get_connected_joypads():
+            viewed_long_enough_label.text = "pause/start to continue"
+        else:
+            viewed_long_enough_label.text = "ESC to continue"
+        return
+    var show_time = 4
+    if not timer.is_stopped():
+        show_time = timer.time_left
+    viewed_long_enough_label.text = "Look! %02d" % show_time
 
 func _input(event: InputEvent) -> void:
-	if not visible:
-		return
-	if not viewed_long_enough:
-		return
+    if not visible:
+        return
+    if not viewed_long_enough:
+        return
 
-	if event.is_action_pressed("jump") or event.is_action_pressed("crouch") or event.is_action_pressed("pause"):
-		animation_player.play(&"RESET")
-		hide()
-		viewed_long_enough = false
-		Events.request_script_pause.emit(false)
+    if event.is_action_pressed("pause"):
+        animation_player.play(&"RESET")
+        hide()
+        viewed_long_enough = false
+        if handle_pause:
+            Events.request_script_pause.emit(false)
+        cheat_code_ui_closed.emit()
 
 func _on_timer_timeout() -> void:
-	viewed_long_enough = true
+    viewed_long_enough = true
